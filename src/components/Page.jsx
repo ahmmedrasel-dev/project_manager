161e51130539ce459aadd2267d7f562e168d5367
ahmmedrasel-dev <rahmmed.info@gmail.com>
2024-10-27 12:06:19 +0600

    create add task modal

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,47 +1,58 @@
+import { useState } from "react";
+import AddTask from "./AddTask";
 import Sidebar from "./Sidebar";
 import Tasklist from "./Tasklist";
 import Topbar from "./Topbar";
 
 const Page = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <div className="flex h-screen">
-      <Sidebar />
+    <>
+      {showModal && <AddTask onClose={() => setShowModal(false)} />}
+      <div className="flex h-screen">
+        <Sidebar />
 
-      <main className="flex-1 overflow-y-auto overflow-x-hidden">
-        {/* Topbar */}
-        <Topbar />
+        <main className="flex-1 overflow-y-auto overflow-x-hidden">
+          <Topbar />
 
-        <div className="mx-auto max-w-7xl p-6">
-          <div className="mb-6 flex items-center justify-between">
-            <h2 className="text-2xl font-bold">Projectify</h2>
-            <div className="flex space-x-2">
-              <button className="flex items-center rounded-md bg-gray-700 px-4 py-2 text-white">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="mr-2"
+          <div className="mx-auto max-w-7xl p-6">
+            <div className="mb-6 flex items-center justify-between">
+              <h2 className="text-2xl font-bold">Projectify</h2>
+              <div className="flex space-x-2">
+                <button
+                  className="flex items-center rounded-md bg-gray-700 px-4 py-2 text-white"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setShowModal(true);
+                  }}
                 >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
-                  <path d="M15 12h-6" />
-                  <path d="M12 9v6" />
-                </svg>
-                Add
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="mr-2"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
+                    <path d="M15 12h-6" />
+                    <path d="M12 9v6" />
+                  </svg>
+                  Add
+                </button>
+              </div>
             </div>
-          </div>
 
-          <Tasklist />
-        </div>
-      </main>
-    </div>
+            <Tasklist />
+          </div>
+        </main>
+      </div>
+    </>
   );
 };
 
--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { getAllTasks } from "../data/data";
 import Task from "./Task";
 
@@ -14,7 +13,7 @@ const Tasklist = () => {
       <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
         <div className="rounded-lg bg-indigo-600 p-4">
           <div className="mb-2 flex items-center justify-between">
-            <h3 className="text-lg font-semibold">To-Do (45)</h3>
+            <h3 className="text-lg font-semibold">To-Do ({todo.length})</h3>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="18"
@@ -47,7 +46,9 @@ const Tasklist = () => {
       <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
         <div className="rounded-lg bg-yellow-500 p-4">
           <div className="mb-2 flex items-center justify-between">
-            <h3 className="text-lg font-semibold">On Progress (45)</h3>
+            <h3 className="text-lg font-semibold">
+              On Progress ({on_progress.length})
+            </h3>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="18"
@@ -71,60 +72,13 @@ const Tasklist = () => {
           {on_progress.map((item) => (
             <Task key={item.id} task={item} />
           ))}
-          {/* <div className="mb-4 rounded-lg bg-gray-800 p-4">
-            <div className="flex justify-between">
-              <h4 className="mb-2 flex-1 font-semibold text-yellow-500">
-                Content Writer
-              </h4>
-              <div className="flex gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="h-4 w-4 cursor-pointer text-zinc-300"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M4 7l16 0" />
-                  <path d="M10 11l0 6" />
-                  <path d="M14 11l0 6" />
-                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                </svg>
-                <svg
-                  className="h-4 w-4 cursor-pointer text-zinc-300"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                  ></path>
-                </svg>
-              </div>
-            </div>
-            <p className="mb-2 text-sm text-zinc-200">
-              Prepare proctor for client meeting
-            </p>
-
-            <p className="mt-6 text-xs text-zinc-400">February 20, 2024</p>
-          </div> */}
         </div>
       </div>
 
       <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
         <div className="rounded-lg bg-teal-500 p-4">
           <div className="mb-2 flex items-center justify-between">
-            <h3 className="text-lg font-semibold">Done (1)</h3>
+            <h3 className="text-lg font-semibold">Done ({done.length})</h3>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="18"
@@ -150,53 +104,6 @@ const Tasklist = () => {
             {done.map((item) => (
               <Task key={item.id} task={item} />
             ))}
-            {/* <div className="mb-4 rounded-lg bg-gray-800 p-4">
-              <div className="flex justify-between">
-                <h4 className="mb-2 font-semibold text-teal-500">
-                  Content Writer
-                </h4>
-                <div className="flex gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-4 w-4 cursor-pointer text-zinc-300"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M4 7l16 0" />
-                    <path d="M10 11l0 6" />
-                    <path d="M14 11l0 6" />
-                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                  </svg>
-                  <svg
-                    className="h-4 w-4 cursor-pointer text-zinc-300"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
-              <p className="mb-2 text-sm text-zinc-200">
-                Make Promotional Ads for Instagram fastos
-              </p>
-
-              <p className="mt-6 text-xs text-zinc-400">February 20, 2024</p>
-            </div> */}
           </div>
         </div>
       </div>
@@ -204,7 +111,7 @@ const Tasklist = () => {
       <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
         <div className="rounded-lg bg-rose-500 p-4">
           <div className="mb-2 flex items-center justify-between">
-            <h3 className="text-lg font-semibold">Revise (1)</h3>
+            <h3 className="text-lg font-semibold">Revise ({revised.length})</h3>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="18"
@@ -229,53 +136,6 @@ const Tasklist = () => {
           {revised.map((item) => (
             <Task key={item.id} task={item} />
           ))}
-          {/* <div className="mb-4 rounded-lg bg-gray-800 p-4">
-            <div className="flex justify-between">
-              <h4 className="mb-2 font-semibold text-rose-500">
-                Content Writer
-              </h4>
-              <div className="flex gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="h-4 w-4 cursor-pointer text-zinc-300"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M4 7l16 0" />
-                  <path d="M10 11l0 6" />
-                  <path d="M14 11l0 6" />
-                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                </svg>
-                <svg
-                  className="h-4 w-4 cursor-pointer text-zinc-300"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                  ></path>
-                </svg>
-              </div>
-            </div>
-            <p className="mb-2 text-sm text-zinc-200">
-              Make Promotional Ads for Instagram fastos
-            </p>
-
-            <p className="mt-6 text-xs text-zinc-400">February 20, 2024</p>
-          </div> */}
         </div>
       </div>
     </div>
